2da9f63f977095c5964cb2417da9afb85a6f6d12
Dxnasty-S <65668690+Dxnasty-S@users.noreply.github.com>
2020-06-01 16:14:06 -0300

    unity
    first commit

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Player.transform.position + offset; 
+    }
+}
---
+++ Assets/Playermovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+    private Rigidbody Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody = GetComponent<Rigidbody>();
+        PlayerMove();   
+    }
+
+    void PlayerMove()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Rigidbody.AddForce(direction * _speed);
+
+        if(horizontalInput == 0 && verticalInput == 0)
+        {
+            Rigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+
+
+
+}
